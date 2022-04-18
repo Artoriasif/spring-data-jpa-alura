5cf94e0da5139b0c6deadc57d991f38736fbd647
Pedro <pedrohh0012@icloud.com>
2022-04-18 17:30:51 -0300

    reparo

--- src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -2,6 +2,7 @@ package br.com.alura.spring.data.orm;
 
 import java.util.List;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
